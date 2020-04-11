76ad495ae5ecaa05ef815354e99d071a9deb635f
dp <davepollan@gmail.com>
2020-04-11 18:14:06 -0400

    project finished

--- dp-employee-track/src/componenets/header/header.js
+++ dp-employee-track/src/componenets/header/header.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function Header() {
     return(
-        <div>Employee Directory</div>
-    )
+        <div><div class="title">Employee Directory</div><div class="intro">Welcome to the Employee Directory!  <br></br>Click the blue table headers to view listings in ascending or descending order.</div></div>
+    )  
 }
