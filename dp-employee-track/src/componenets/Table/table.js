010c745bd6f47beb92a11c7852ed7d0e513a392a
dp <davepollan@gmail.com>
2020-04-10 23:40:30 -0400

    basic table in place

---
+++ dp-employee-track/src/componenets/Table/table.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function Table() {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Nationality</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>dsdgs</th>
+                    <th>nndfnfdl</th>
+                    <th>134</th>
+                    <th>ddsg</th> 
+                </tr>
+            </tbody>
+        </table>
+    )
+}
